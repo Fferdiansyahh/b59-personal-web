8078ce7d19a2368c270acc648d3a01c4576fd234
fferdiansyahh <ansyah.ferdi5@gmail.com>
2025-01-12 12:03:10 +0700

    fix vercel

--- Asset/js/testimonial.js
+++ Asset/js/testimonial.js
@@ -4,35 +4,35 @@ let testimonials = [
     rating: 5,
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in gravida sem, condimentum sagittis justo. Vestibulum nulla felis, fringilla at molestie in, laoreet ac libero. ",
-    image: "project/Project_4.png",
+    image: "Project_4.png",
   },
   {
     author: "User",
     rating: 4,
     content:
       "Mauris rhoncus tempor lorem in convallis. Integer ornare massa vitae arcu tristique tristique. Nullam non diam at velit egestas auctor faucibus ut ex. Curabitur bibendum tincidunt arcu et rutrum. ",
-    image: "project/Project_4.png",
+    image: "Project_4.png",
   },
   {
     author: "Joko",
     rating: 3,
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea reiciendis qui molestias blanditiis inventore reprehenderit nesciunt sequi pariatur quaerat? Error?",
-    image: "project/Project_4.png",
+    image: "Project_4.png",
   },
   {
     author: "Iqbal",
     rating: 4,
     content:
       "Vestibulum pulvinar pretium fermentum. Nullam at felis maximus, iaculis nunc in, rutrum nunc. Fusce a velit eget magna rutrum dignissim. Cras venenatis augue tellus, id volutpat ante blandit hendrerit. ",
-    image: "project/Project_4.png",
+    image: "Project_4.png",
   },
   {
     author: "Tsabit",
     rating: 5,
     content:
       "Vestibulum pulvinar pretium fermentum. Nullam at felis maximus, iaculis nunc in, rutrum nunc. Fusce a velit eget magna rutrum dignissim. Cras venenatis augue tellus, id volutpat ante blandit hendrerit. ",
-    image: "project/Project_4.png",
+    image: "Project_4.png",
   },
 ];
 
